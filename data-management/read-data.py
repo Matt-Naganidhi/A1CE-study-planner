6c68b83d449af5cc8ffabaad25f725dfdc1bbb55
Supakorn Etitum <setitum@cmkl.ac.th>
2024-10-21 20:06:40 +0700

    Turned the code into function and added sample input file

--- data-management/read-data.py
+++ data-management/read-data.py
@@ -1,17 +1,23 @@
+#This source code is used to read txt files and convert them into lists
+#Inputs: input file path, output file path
+#Outputs: None
 #Author Supakorn Etitum (JackJack)
 
 import pandas as pd
 import csv
 
 
-read_file = pd.read_csv("/Users/jayj/software engineering process/JackJackRoadmap.txt", delimiter='|', skiprows=[0, 1])
+def openFile(file_path,output_path):
+    read_file = pd.read_csv(file_path, delimiter='|', skiprows=2, skipfooter=2, engine='python')
 
-read_file.to_csv("/Users/jayj/software engineering process/output.csv", index=False)
+    read_file.to_csv(output_path, index=False)
 
-with open("/Users/jayj/software engineering process/output.csv", mode='r') as file:
-    csvFile = csv.reader(file)
-    for lines in csvFile:
-        if lines == 2:
-            continue
-        else:
-            print(lines)
+    with open(output_path, mode='r') as file:
+        csvFile = csv.reader(file)
+        for lines in csvFile:
+            if lines == 2:
+                continue
+            else:
+                print(lines)
+                
+openFile("/Users/jayj/software engineering process/JackJackRoadmap.txt","/Users/jayj/software engineering process/output.csv")
