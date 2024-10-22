fd73200519ab9ef04b88082737118822010897ab
Matt Naganidhi <66015573+Matt-Naganidhi@users.noreply.github.com>
2024-10-22 22:03:53 +0700

    add task management

--- source_code/Task-management/Task.py
+++ source_code/Task-management/Task.py
@@ -1,4 +1,16 @@
 #This source file is made to add, modfiy, and delete tasks
-#
-def Task_info ():
+#inputs device date data, roadmap data
+#Author Im and Matt
+import os
+import csv
+import pandas as pd 
+def Task_info (roadmap_information, current_device_date, user_input_array):
+    #task name
+    #task duration
+    task_duration = user_input_array[end_date] - current_device_date
+    #task description
+def modify_task ():
+def user_input ()
+    
+    
     
