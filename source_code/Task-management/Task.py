ad293938b3000289c92f81312f6b766894257e44
ueaangkunsr <usrivir@cmkl.ac.th>
2024-10-23 19:10:53 +0700

    Update Task.py with user_input

--- source_code/Task-management/Task.py
+++ source_code/Task-management/Task.py
@@ -4,13 +4,23 @@
 import os
 import csv
 import pandas as pd 
+
 def Task_info (roadmap_information, current_device_date, user_input_array):
     #task name
     #task duration
     task_duration = user_input_array[end_date] - current_device_date
     #task description
-def modify_task ():
-def user_input ()
+def modify_task():
+
+#get task id, new competency name, skill code, skill name
+def user_input():
+    task_id = input("Enter the Task ID you want to modify (competency_code): ") 
+    new_competency_name = input("Enter new competency name (leave blank to keep unchanged): ")  
+    new_skill_code = input("Enter new skill code (leave blank to keep unchanged): ") 
+    new_skill_name = input("Enter new skill name (leave blank to keep unchanged): ") 
+    
+    # Return the user inputs as a tuple
+    return task_id, new_competency_name, new_skill_code, new_skill_name
     
     
     
