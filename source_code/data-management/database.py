7bcd2e9a8b02e29672cb1faa0d249235f1375f84
golCMKL <tjianwa@cmkl.ac.th>
2024-11-07 22:07:33 +0700

    changed to funciton

--- source_code/data-management/database.py
+++ source_code/data-management/database.py
@@ -6,41 +6,43 @@
 import sqlite3
 import pandas as pd
 
+
 # Create/connect to the database
-con = sqlite3.connect('roadmap.db')
-# Create a cursor to execute commands on the database
-cursor = con.cursor()
+def init_database(inputfile):
+    con = sqlite3.connect('roadmap.db')
+    # Create a cursor to execute commands on the database
+    cursor = con.cursor()
 
-# Read the CSV file into a DataFrame
-df = pd.read_csv('/Users/jayj/A1CE-study-planner/source_code/data-management/output.csv')
+    # Read the CSV file into a DataFrame
+    df = pd.read_csv(inputfile)
 
-# Create the table (if it doesn't exist already)
-cursor.execute('''CREATE TABLE IF NOT EXISTS roadmap (competency_code TEXT,competency_name TEXT,skill_code TEXT,skill_name TEXT)''')
+    # Create the table (if it doesn't exist already)
+    cursor.execute('''CREATE TABLE IF NOT EXISTS roadmap (competency_code TEXT,competency_name TEXT,skill_code TEXT,skill_name TEXT)''')
 
-df.columns = df.columns.str.strip()
-print(df.columns) 
+    df.columns = df.columns.str.strip()
+    print(df.columns)
 
-print(df['title'])
+    print(df['title'])
 
 
 
 # Insert the data from the DataFrame into the table
-for index, row in df.iterrows():
-    cursor.execute('''
-    INSERT INTO roadmap (competency_code, title, assessed_skill_code, title)
-    VALUES (?, ?, ?, ?)
-    ''', (row['competency_code'], row['competency_name'], row['skill_code'], row['skill_name']))
+    for index, row in df.iterrows():
+        cursor.execute('''
+        INSERT INTO roadmap (competency_code, title, assessed_skill_code, title)
+        VALUES (?, ?, ?, ?)
+        ''', (row['competency_code'], row['competency_name'], row['skill_code'], row['skill_name']))
 
-# Commit the transaction to save changes
-con.commit()
+    # Commit the transaction to save changes
+    con.commit()
 
-# Fetch and print all rows from the 'roadmap' table
-cursor.execute("SELECT * FROM roadmap")
-rows = cursor.fetchall()
+    # Fetch and print all rows from the 'roadmap' table
+    cursor.execute("SELECT * FROM roadmap")
+    rows = cursor.fetchall()
 
 # Print all rows
-for row in rows:
-    print(row)
+# for row in rows:
+#     print(row)
 
 # Close the connection
-con.close()
+    con.close()
